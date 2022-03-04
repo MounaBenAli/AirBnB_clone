33a186913234fac119e8502b60fafbe43912cb87
MounaBenAli <benalimouna1986@gmail.com>
2022-03-04 14:46:58 +0100

    testing

---
+++ test_save_reload_user.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+
+print("-- Create a new User --")
+my_user = User()
+my_user.first_name = "Betty"
+my_user.last_name = "Bar"
+my_user.email = "airbnb@mail.com"
+my_user.password = "root"
+my_user.save()
+print(my_user)
+
+
+print("__state__")
+my_state = State()
+my_state.name = "California"
+print(my_state)
+
+print("__city__")
+my_city = City()
+State.id = ""
+my_city.name ="San Diego"
+print(my_city)
