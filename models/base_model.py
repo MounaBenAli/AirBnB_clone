e3c5f8e947c5437fa17a5095580c5c973292cee1
louaysaidi22 <louaysaidi18@gmail.com>
2022-03-04 11:30:18 +0100

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,55 +1,42 @@
 #!/usr/bin/python3
-"""
-base model
-"""
+"""BASE_MODEL"""
 import models
 import uuid
 from datetime import datetime
 
 
 class BaseModel():
-    """
-    Base model that defines all common attributes/methods for all the classes
-    """
+    """Base model class"""
 
     def __init__(self, *args, **kwargs):
-        """
-        __init__ methode
-        """
+        """initialization"""
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
         if kwargs:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    v = datetime.strptime(v, time_format)
-                if k != "__class__":
-                    setattr(self, k, v)
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, time_format)
+                if key != "__class__":
+                    setattr(self, key, value)
         else:
             models.storage.new(self)
 
     def __str__(self):
-        """
-        print with this format : [<class name>] (<self.id>) <self.__dict__>
-        """
+        """print this format[<class name>] (<self.id>) <self.__dict__>"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
                                      self.__dict__)
 
     def save(self):
-        """
-        updates the public instance attribute
-        updated_at with the current datetime
-        """
+        """updates the public instance attribute"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """
-        returns a dictionary containing all keys/values of __dict__
-        """
-        d = dict(**self.__dict__)
-        d['created_at'] = self.created_at.isoformat()
-        d['updated_at'] = self.updated_at.isoformat()
-        return (d)
+        """returns dictionary"""
+        new_dic = dict(**self.__dict__)
+        new_dic['created_at'] = self.created_at.isoformat()
+        new_dic['updated_at'] = self.updated_at.isoformat()
+        return (new_dic)
