9bfc70478f59df96e3b8d975c788b8b7ade982b9
louaysaidi22 <louaysaidi18@gmail.com>
2022-03-04 21:25:40 +0100

    city.py

--- models/city.py
+++ models/city.py
@@ -7,9 +7,9 @@ from models.base_model import BaseModel
 class City(BaseModel):
     """Represent a City.
     Attributes:
-        state_id: (str): It will be the State.id    
+        state_id: (str): It will be the State.id
         name: (str): The name of the city.
     """
 
-    state_id =""
-    name =""
+    state_id = ""
+    name = ""
