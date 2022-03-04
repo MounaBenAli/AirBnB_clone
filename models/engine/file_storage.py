8a8f424583a08579e28a792979ad1c82696d61a6
louaysaidi22 <louaysaidi18@gmail.com>
2022-03-04 11:34:49 +0100

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,51 +1,39 @@
 #!/usr/bin/python3
-"""
-file storage module
-"""
+"""FILE STORAGE"""
 import json
 from models.base_model import BaseModel
 
 
 class FileStorage:
-    """
-    file storage class
-    """
+    """File Ftorage Class"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        returns the dictionary __objects of the file
-        """
+        """returns dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        """
-         sets in __objects the obj with key <obj class name>.id
-        """
+        """sets __objects"""
         if obj:
             self.__objects["{}.{}".format(obj.__class__.__name__,
                                           obj.id)] = obj
 
     def save(self):
-        """
-        serializes __objects to the JSON file (path: __file_path)
-        """
-        new_dict = {}
-        for k, v in self.__objects.items():
-            new_dict[k] = v.to_dict()
+        """serializes __objects to the JSON file        """
+        my_dic = {}
+        for key, value in self.__objects.items():
+            my_dic[key] = value.to_dict()
         with open(self.__file_path, "w", encoding="UTF-8") as f:
-            json.dump(new_dict, f)
+            json.dump(my_dic, f)
 
     def reload(self):
-        """
-        deserializes the JSON file to __objects from the file
-        """
+        """deserializes the JSON file to __objects """
         try:
             with open(self.__file_path, "r", encoding="UTF-8") as f:
                 obj = json.load(f)
-            for k, v in obj.items():
-                class_name = k.split('.')[0]
-                self.__objects[k] = eval(class_name)(**v)
+            for key, value in obj.items():
+                class_name = key.split('.')[0]
+                self.__objects[key] = eval(class_name)(**value)
         except BaseException:
             pass
