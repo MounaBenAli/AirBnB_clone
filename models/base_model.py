a4a3d6449c102610a7f1a21c0947b900cdc2ab45
MounaBenAli <benalimouna1986@gmail.com>
2022-03-04 14:24:29 +0100

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,47 @@
 #!/usr/bin/python3
-"""BASE_MODEL"""
+"""Defines the BaseModel class."""
+
 import models
 import uuid
 from datetime import datetime
 
 
 class BaseModel():
-    """Base model class"""
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """initialization"""
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
         if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, time_format)
-                if key != "__class__":
-                    setattr(self, key, value)
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, time_format)
+                if k != "__class__":
+                    setattr(self, k, v)
         else:
             models.storage.new(self)
 
     def __str__(self):
-        """print this format[<class name>] (<self.id>) <self.__dict__>"""
+        """Return the print/str representation of the BaseModel instance."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
                                      self.__dict__)
 
     def save(self):
-        """updates the public instance attribute"""
+        """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """returns dictionary"""
-        new_dic = dict(**self.__dict__)
-        new_dic['created_at'] = self.created_at.isoformat()
-        new_dic['updated_at'] = self.updated_at.isoformat()
-        return (new_dic)
+        """Returns a dictionary containing all keys/values of __dict__"""
+        d = dict(**self.__dict__)
+        d['created_at'] = self.created_at.isoformat()
+        d['updated_at'] = self.updated_at.isoformat()
+        return (d)
