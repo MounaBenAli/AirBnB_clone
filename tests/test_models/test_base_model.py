d85f63df47d72d7b074bbf5e43e0af6bd00fed5f
MounaBenAli <benalimouna1986@gmail.com>
2022-03-06 11:59:38 +0100

    updating tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -36,13 +36,48 @@ class Test_BaseModel_Docs(unittest.TestCase):
 class Test_BaseModel_All(unittest.TestCase):
     """Tests for BaseModel"""
 
+    @classmethod
+    def setUp(cls):
+        """Standard setUp()"""
+        cls.model = BaseModel()
+
+    @classmethod
+    def tearDown(cls):
+        """Standard tearDown()"""
+        del cls.model
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
     def init(self):
         """Test instantiation"""
-        pass
+        self.assertIsInstance(self.model, BaseModel)
+
+    def test_no_args_instantiates(self):
+        """Test no args"""
+        self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_args_unused(self):
+        """Test unused args"""
+        my_model = BaseModel(None)
+        self.assertNotIn(None, my_model.__dict__.values())
+
+    def test_attr(self):
+        """Test updated_at and created_at"""
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_unique_id(self):
+        """Test for uuid"""
+        id1 = BaseModel()
+        id2 = BaseModel()
+        self.assertNotEqual(id1.id, id2.id)
 
     def test_save(self):
         """Test save()"""
-        pass
+        self.model.save()
+        self.assertNotEqual(self.model.created_at, self.model.updated_at)
 
     def test_not_empty(self):
         """ Test if the json file is not empty """
@@ -50,7 +85,10 @@ class Test_BaseModel_All(unittest.TestCase):
 
     def test_to_dict(self):
         """Test to_dict()"""
-        pass
+        model_dict = self.model.to_dict()
+        self.assertEqual(self.model.__class__.__name__, 'BaseModel')
+        self.assertIsInstance(model_dict['created_at'], str)
+        self.assertIsInstance(model_dict['updated_at'], str)
 
 
 if __name__ == '__main__':
