954637af51d190e81e152a72d3633e0841ffa383
MounaBenAli <benalimouna1986@gmail.com>
2022-03-06 12:42:34 +0100

    updating tests amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""unittests for amenity.py.
+"""unittests for user.py.
 
 Unittest classes:
     Test_Amenity_Docs
@@ -7,14 +7,9 @@ Unittest classes:
 """
 import unittest
 import os
-import uuid
 import pep8
-from datetime import datetime
-from datetime import time
-from models.base_model import BaseModel
-import models
 from models.__init__ import storage
-from models.user import User
+from models.base_model import BaseModel
 from models.amenity import Amenity
 
 
@@ -34,9 +29,63 @@ class Test_Amenity_Docs(unittest.TestCase):
 class Test_Amenity_All(unittest.TestCase):
     """Tests for Amenity"""
 
-    def test_name(self):
-        """Test Amenity  name"""
-        pass
+    @classmethod
+    def setUp(cls):
+        """Standard setUp()"""
+        cls.Amenity = Amenity()
+        cls.Amenity.name = "travel pack"
+
+    @classmethod
+    def tearDown(cls):
+        """Standard tearDown()"""
+        del cls.Amenity
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_subclass(self):
+        """Test for inheritance from BaseModel"""
+        self.assertTrue(issubclass(self.Amenity.__class__, BaseModel), True)
+
+    def test_no_args_instantiates(self):
+        """Test no args"""
+        self.assertEqual(Amenity, type(Amenity()))
+
+    def test_args_unused(self):
+        """Test unused args"""
+        my_Amenity = Amenity(None)
+        self.assertNotIn(None, my_Amenity.__dict__.values())
+
+    def test_attr(self):
+        """Test updated_at and created_at"""
+        self.assertTrue('id' in self.Amenity.__dict__)
+        self.assertTrue('created_at' in self.Amenity.__dict__)
+        self.assertTrue('updated_at' in self.Amenity.__dict__)
+        self.assertTrue('name' in self.Amenity.__dict__)
+
+    def test_unique_id(self):
+        """Test for uuid"""
+        us_id1 = Amenity()
+        us_id2 = Amenity()
+        self.assertNotEqual(us_id1.id, us_id2.id)
+
+    def test_save(self):
+        """Test save()"""
+        self.Amenity.save()
+        self.assertNotEqual(self.Amenity.created_at, self.Amenity.updated_at)
+
+    def test_to_dict(self):
+        """Test to_dict()"""
+        self.assertEqual('to_dict' in dir(self.Amenity), True)
+
+    def test_to_dict_type(self):
+        """Test to_dict() type"""
+        self.assertTrue(dict, type(Amenity().to_dict()))
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 if __name__ == '__main__':
